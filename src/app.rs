688309998ac4611cfacd81751d67f9c443077cdf
eci <eci@guylemon.com>
2026-04-09 07:23:01 -0500

    Add input_reader (Read) to read text input

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,31 @@
-struct Todo;
-struct AppError;
+use std::io::Read;
 
-pub struct App;
+#[derive(Debug)]
+pub struct Todo;
 
-impl App {
-    pub fn new() -> Self {
-       Self 
+pub struct App<R> {
+    input_reader: R,
+}
+
+impl<R> App<R>
+where
+    R: Read,
+{
+    pub fn new(input_reader: R) -> Self {
+        Self { input_reader }
+    }
+
+    pub fn run(mut self) -> Result<Todo, Todo> {
+        // Read the raw source document text. The application will create a graph structure from
+        // the entities contained within the text.
+        // TODO do something with this value after output sketch is ready
+        let mut raw_text = String::new();
+        self.input_reader
+            .read_to_string(&mut raw_text)
+            .map_err(|_| Todo)?;
+
+        // The application will transform the graph to a JSON format used by Cytoscape.js and write
+        // it to a file for inclusion in a static html document. https://js.cytoscape.org/#demos
+        Ok(Todo)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod app;
 
+use std::{io::Cursor, process};
+
 use crate::app::App;
 
 fn main() {
-    App::new();
+    // TODO use stdin in near future. To support type-driven development, use this stub type.
+    let input_reader = Cursor::new(Vec::new());
+    let application = App::new(input_reader);
+
+    // TODO no need to use debug when an error type is in place.
+    if let Err(e) = application.run() {
+        eprintln!("{e:?}");
+        process::exit(1)
+    }
     println!("Hello, world!");
 }
