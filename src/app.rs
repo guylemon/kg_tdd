7b90b478bb362a3276b2f1ca51d7d19d6d4f161e
eci <eci@guylemon.com>
2026-04-09 07:34:25 -0500

    Add graph_writer (Write) to write output
    - At this point, the application has modeled minimal I/O, and I can
      proceed to discovering domain types.

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,29 @@
 use std::io::Read;
+use std::io::Write;
+
+const TODO_STRING: &str = "";
 
 #[derive(Debug)]
 pub struct Todo;
 
-pub struct App<R> {
+pub struct App<R, W> {
     input_reader: R,
+    graph_writer: W,
 }
 
-impl<R> App<R>
+impl<R, W> App<R, W>
 where
     R: Read,
+    W: Write,
 {
-    pub fn new(input_reader: R) -> Self {
-        Self { input_reader }
+    pub fn new(input_reader: R, graph_writer: W) -> Self {
+        Self {
+            input_reader,
+            graph_writer,
+        }
     }
 
-    pub fn run(mut self) -> Result<Todo, Todo> {
+    pub fn run(mut self) -> Result<(), Todo> {
         // Read the raw source document text. The application will create a graph structure from
         // the entities contained within the text.
         // TODO do something with this value after output sketch is ready
@@ -24,8 +32,11 @@ where
             .read_to_string(&mut raw_text)
             .map_err(|_| Todo)?;
 
-        // The application will transform the graph to a JSON format used by Cytoscape.js and write
-        // it to a file for inclusion in a static html document. https://js.cytoscape.org/#demos
-        Ok(Todo)
+        // Write the serialized graph
+        self.graph_writer
+            .write(TODO_STRING.as_bytes())
+            .map_err(|_| Todo)?;
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ use crate::app::App;
 fn main() {
     // TODO use stdin in near future. To support type-driven development, use this stub type.
     let input_reader = Cursor::new(Vec::new());
-    let application = App::new(input_reader);
+
+    // TODO use a file in the near future. The application will transform the graph to a JSON
+    // format used by Cytoscape.js and write it to a file for inclusion in a static html document.
+    // https://js.cytoscape.org/#demos
+    let graph_writer = Cursor::new(Vec::new());
+
+    let application = App::new(input_reader, graph_writer);
 
     // TODO no need to use debug when an error type is in place.
     if let Err(e) = application.run() {
