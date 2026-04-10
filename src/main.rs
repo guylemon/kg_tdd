e809b2eb7baab5621f58ce6a205816cbd841f76d
Guy Lemon <guylemon@users.noreply.github.com>
2026-04-10 08:20:52 -0500

    Create type-driven sketch (#1)
    feat: Create single-file type sketch of ingestion workflow
    
    - Adds a type-driven workflow that compiles and runs. The application does not actually accomplish its goal, but the workflow functions and most domain types are stubbed in, along with some transformation logic.
    - CAVEATS: There are LLM and tokenizer traits and supporting types yet to be discovered, so the total surface area is not fully known.
    
    ---------
    
    Co-authored-by: eci <eci@guylemon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod app;
+
+use std::{io::Cursor, process};
+
+use crate::app::App;
+use crate::app::MaxConcurrency;
+
 fn main() {
+    // TODO use stdin in near future. To support type-driven development, use this stub type.
+    let input_reader = Cursor::new(Vec::new());
+
+    // TODO use a file in the near future. The application will transform the graph to a JSON
+    // format used by Cytoscape.js and write it to a file for inclusion in a static html document.
+    // https://js.cytoscape.org/#demos
+    let cytoscape_writer = Cursor::new(Vec::new());
+
+    let max_concurrency = MaxConcurrency(4);
+
+    let application = App::new(input_reader, cytoscape_writer, max_concurrency);
+
+    // TODO no need to use debug when an error type is in place.
+    if let Err(e) = application.run() {
+        eprintln!("{e:?}");
+        process::exit(1)
+    }
     println!("Hello, world!");
 }
